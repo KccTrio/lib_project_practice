9dd44c1cbc86fc06c920655d24873c2336e6a5ea
yelimslee <leeyl1595@naver.com>
2024-08-28 11:35:39 +0900

    Create Yelim

---
+++ src/main/java/com/kcc/lib/domain/Yelim.java
@@ -0,0 +1,14 @@
+package com.kcc.lib.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Yelim {
+	private int age;
+	private String name;
+	private String office;
+}
